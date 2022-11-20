f59feb042340ebe146f78f617acd757286faa029
griffen <savadoux@et.esiea.fr>
2022-11-20 19:41:06 +0100

    <fix> : div logo trop grande

--- index.php
+++ index.php
@@ -18,7 +18,6 @@
             localStorage.setItem("scrollpos", window.scrollY);
         };
     </script>
-
     <body style="color:white">
     <?php
 
@@ -103,15 +102,11 @@
     if ($result_can) { ?>
 
         <div class="pub_gauche" >
-            ici
-            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9998748839382972"
-                    crossorigin="anonymous"></script>
+            test
         </div >
 
         <div class="pub_droite">
-            labas
-            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9998748839382972"
-                    crossorigin="anonymous"></script>
+            test2
         </div>
     <?php } ?>
 
@@ -176,7 +171,7 @@
         <?php
     }
     if (isset($_POST["message"]) && !empty(trim(str_replace("'","\'",$_POST["message"])))) {
-        $message_replace = htmlspecialchars(str_replace("'","\'",$_POST["message"]));
+        $message_replace = str_replace("'","\'",$_POST["message"]);
         insert_fields('post', ["ID_user" => $result_can['id'], "post" => $message_replace,"username_source" => $result_can['username'], "username_destinataire" => $guest]);  //mettre l'id user dans la requets
         echo '<meta http-equiv="refresh" content="0">';
     }
